348ede99a0dc44d64600384bf20b037078fce6eb
qianachen8 <qianachen8@gmail.com>
2024-01-11 14:35:22 -0800

    lab1

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+/*  hello_xiao    A first sketch for the Seeeduino XIAO in PlatformIO
+  
+    This example code is in the public domain.*
+  
+    Michel Deslierres  June 15, 2020 
+  */
+  
+  #include <Arduino.h>  // needed in PlatformIO*
+  
+  void setup() {
+  }
+  
+  void loop() {
+    Serial.println("Hello XIAO!");
+    delay(2000);
+  }
